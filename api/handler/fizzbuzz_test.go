8d5158f454798e20f7495d943ed11684d8b71f0e
maxime marcuccilli <maxime.marcuccilli@epitech.eu>
2022-11-04 11:36:01 +0100

    remove working test

--- api/handler/fizzbuzz_test.go
+++ api/handler/fizzbuzz_test.go
@@ -13,16 +13,7 @@ func TestFizzBuzzHandler(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-	}{
-		name: "Test FizzBuzz Query",
-		args: args{
-			int1:  "3",
-			int2:  "5",
-			limit: "15",
-			str1:  "toto",
-			str2:  "foo",
-		},
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			FizzBuzzHandler(tt.args.c)
