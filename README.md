163e9ab5ee10d44a6f242ff49fd2ebffaab1ffcd
maxime marcuccilli <maxime.marcuccilli@epitech.eu>
2022-11-04 18:18:56 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# lbcTest
+<!-- GETTING STARTED -->
+## Getting Started
+
+First of all, clone the repository with git.
+  ```sh
+  git clone https://github.com/MaximeMcs/lbcTest.git
+  ```
+
+### Prerequisites
+
+
+* Install Golang https://go.dev/doc/install
+* Install Docker https://docs.docker.com/engine/install/
+
+### Installation
+
+1. Launch docker containers
+   ```sh
+   docker-compose up
+   ```
+2. Once everything up, get API Documentation on http://localhost:8000/swagger/index.html
+3. Play with the API on Swagger or with your favorite API tool.
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+1. fizzbuzz endpoint: send 5 parameters and get a fizzbuzzed string.
+http://localhost:8000/fizzbuzz?int1=3&int2=5&limit=100&str1=toto&str2=test
+
+2. queries endpoint: get the most popular query and its number of hits.
+http://localhost:8000/queries
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- api/handler/fizzbuzz_test.go
+++ api/handler/fizzbuzz_test.go
@@ -1,10 +1,61 @@
 package handler
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
+
+func Test_saveQuery(t *testing.T) {
+	type args struct {
+		query string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := saveQuery(tt.args.query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("saveQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("saveQuery() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fizzBuzz(t *testing.T) {
+	type args struct {
+		a     int
+		b     int
+		limit int
+		str1  string
+		str2  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fizzBuzz(tt.args.a, tt.args.b, tt.args.limit, tt.args.str1, tt.args.str2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fizzBuzz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestFizzBuzzHandler(t *testing.T) {
 	type args struct {
@@ -13,7 +64,9 @@ func TestFizzBuzzHandler(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-	}{}
+	}{
+		// TODO: Add test cases.
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			FizzBuzzHandler(tt.args.c)
