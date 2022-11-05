800db9e39bd77545215f4ad68ea3a75d416b56b0
maxime marcuccilli <maxime.marcuccilli@epitech.eu>
2022-11-05 16:34:22 +0100

    unit test example with fizzbuzz

--- api/handler/fizzbuzz_test.go
+++ api/handler/fizzbuzz_test.go
@@ -3,35 +3,7 @@ package handler
 import (
 	"reflect"
 	"testing"
-
-	"github.com/gin-gonic/gin"
 )
-
-func Test_saveQuery(t *testing.T) {
-	type args struct {
-		query string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int64
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := saveQuery(tt.args.query)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("saveQuery() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("saveQuery() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func Test_fizzBuzz(t *testing.T) {
 	type args struct {
@@ -46,30 +18,23 @@ func Test_fizzBuzz(t *testing.T) {
 		args args
 		want []string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test fizzBuzz",
+			args: args{
+				a:     2,
+				b:     3,
+				limit: 6,
+				str1:  "test",
+				str2:  "toto",
+			},
+			want: []string{"1", "test", "toto", "test", "5", "testtoto"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fizzBuzz(tt.args.a, tt.args.b, tt.args.limit, tt.args.str1, tt.args.str2); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fizzBuzz() = %v, want %v", got, tt.want)
 			}
-		})
-	}
-}
-
-func TestFizzBuzzHandler(t *testing.T) {
-	type args struct {
-		c *gin.Context
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			FizzBuzzHandler(tt.args.c)
 		})
 	}
 }
